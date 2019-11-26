292e9b5592c3ef3120c1c3ca5218d73f88aabff5
Assala <asallaamuhsen@gmail.com>
2019-11-26 17:05:57 +0200

    measures

--- BearingMachineSimulation/Form1.Designer.cs
+++ BearingMachineSimulation/Form1.Designer.cs
@@ -63,7 +63,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1238, 374);
+            this.tabPage2.Size = new System.Drawing.Size(1238, 652);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "ProposedMethod";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -72,7 +72,7 @@
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1238, 374);
+            this.tabPage3.Size = new System.Drawing.Size(1238, 652);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Measures";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -96,6 +96,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
--- BearingMachineSimulation/Form1.cs
+++ BearingMachineSimulation/Form1.cs
@@ -17,9 +17,9 @@ namespace BearingMachineSimulation
         public Form1()
         {
             InitializeComponent();
-            List<string> tests=new List<string>() { Constants.FileNames.TestCase1, Constants.FileNames.TestCase2, Constants.FileNames.TestCase3 };
+            List<string> tests = new List<string>() { Constants.FileNames.TestCase1, Constants.FileNames.TestCase2, Constants.FileNames.TestCase3 };
             comboBox1.DataSource = tests;
-           
+
 
 
             //results = TestingManager.Test(system, Constants.FileNames.TestCase3);
@@ -45,10 +45,10 @@ namespace BearingMachineSimulation
                 int k = 0;
                 for (int i = 0; i < table.Count; i++)//add rows
                 {
-                    if (table[i].Bearing.Index == j + 1&&grid.Rows.Count>k)
+                    if (table[i].Bearing.Index == j + 1 && grid.Rows.Count > k)
                     {
 
-                        
+
                         grid.Rows[k].Cells["index"].Value = k + 1;
                         grid.Rows[k].Cells["RH" + j].Value = table[i].Bearing.RandomHours;
                         grid.Rows[k].Cells["H" + j].Value = table[i].Bearing.Hours;
@@ -80,20 +80,46 @@ namespace BearingMachineSimulation
             for (int i = 0; i < table.Count; i++)//add rows
             {
                 grid.Rows[i].Cells["index"].Value = i + 1;
-                for(int j=0;j<noOfBearing;j++)
+                for (int j = 0; j < noOfBearing; j++)
                 {
-                    grid.Rows[i].Cells["RH"+j].Value = table[i].Bearings[j].RandomHours;
-                    grid.Rows[i].Cells["H"+j].Value = table[i].Bearings[j].Hours;
+                    grid.Rows[i].Cells["RH" + j].Value = table[i].Bearings[j].RandomHours;
+                    grid.Rows[i].Cells["H" + j].Value = table[i].Bearings[j].Hours;
                 }
                 grid.Rows[i].Cells["F"].Value = table[i].FirstFailure;
                 grid.Rows[i].Cells["AH"].Value = table[i].AccumulatedHours;
                 grid.Rows[i].Cells["RD"].Value = table[i].RandomDelay;
                 grid.Rows[i].Cells["D"].Value = table[i].Delay;
-                
+
             }
-        
+
             return grid;
 
+        }
+
+        DataGridView performanceMeasures(PerformanceMeasures current, PerformanceMeasures proposed)
+        {
+            DataGridView grid = new DataGridView();
+            grid.Columns.Add("", "");
+            grid.Columns.Add("BC", "BearingCost");
+            grid.Columns.Add("DelC", "DelayCost");
+            grid.Columns.Add("DowC", "DowntimeCost");
+            grid.Columns.Add("RC", "RepairPersonCost");
+            grid.Columns.Add("TC", "TotalCost");
+            grid.Rows.Add(2);
+            grid.Rows[0].Cells[""].Value = "CurrentMethhod";
+            grid.Rows[0].Cells["BC"].Value = current.BearingCost;
+            grid.Rows[0].Cells["DelC"].Value = current.DelayCost;
+            grid.Rows[0].Cells["DowC"].Value = current.DowntimeCost;
+            grid.Rows[0].Cells["RC"].Value = current.RepairPersonCost;
+            grid.Rows[0].Cells["TC"].Value = current.TotalCost;
+            grid.Rows[1].Cells[""].Value = "ProposedMethod";
+            grid.Rows[1].Cells["BC"].Value = proposed.BearingCost;
+            grid.Rows[1].Cells["DelC"].Value = proposed.DelayCost;
+            grid.Rows[1].Cells["DowC"].Value = proposed.DowntimeCost;
+            grid.Rows[1].Cells["RC"].Value = proposed.RepairPersonCost;
+            grid.Rows[1].Cells["TC"].Value = proposed.TotalCost;
+
+            return grid;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -103,7 +129,7 @@ namespace BearingMachineSimulation
             system.startSimulation(file);
             string results = TestingManager.Test(system, file);
             MessageBox.Show(results);
-            if (tabPage1.Controls.Count > 0) 
+            if (tabPage1.Controls.Count > 0)
                 tabPage1.Controls.RemoveAt(0);
             tabPage1.Controls.Add(currentGridView(system.CurrentSimulationTable, system.NumberOfBearings));
             tabPage1.Controls[0].Width = tabPage1.Width;
@@ -114,6 +140,18 @@ namespace BearingMachineSimulation
             tabPage2.Controls.Add(ProposedGridView(system.ProposedSimulationTable, system.NumberOfBearings));
             tabPage2.Controls[0].Width = tabPage2.Width;
             tabPage2.Controls[0].Height = tabPage2.Height;
+
+            if (tabPage3.Controls.Count > 0)
+                tabPage3.Controls.RemoveAt(0);
+            tabPage3.Controls.Add(performanceMeasures(system.CurrentPerformanceMeasures, system.ProposedPerformanceMeasures));
+            tabPage3.Controls[0].Width = tabPage3.Width;
+            tabPage3.Controls[0].Height = tabPage3.Height;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
